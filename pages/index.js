30c4aee1e54d162a162325349df8638489fc5701
Nico Dann <nicodann@gmail.com>
2023-10-27 22:22:11 -0400

    Add page, Links and CSS.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* Learn <a href="https://nextjs.org">Next.js1!</a> */}
+          This <a href='/posts/first-post'>is an 'a' tag!</a><br/>
+          Read <Link href='/posts/first-post'>this page!</Link>
         </h1>
 
         <p className={styles.description}>
